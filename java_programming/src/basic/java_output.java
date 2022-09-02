54d87fabc7e100b6d4380c9d31fee1cd59261bd4
nabilazulien <nabilabasya.business@gmail.com>
2022-09-02 11:56:43 +0700

    java_output

---
+++ java_programming/src/basic/java_output.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class java_output {
+    public static void main(String[] args) {
+        System.out.println(25 * 5);
+        System.out.println(87 + 3);
+        System.out.println(400 / 40);
+        System.out.println(23 * 2 + 14 / 2);
+        System.out.println(1500 / 5);
+        System.out.println(35 * 3);
+    }
+}
