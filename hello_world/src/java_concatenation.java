7ef163180e0a792039721a9cc040521ab74f61b2
nabilazulien <nabilabasya.business@gmail.com>
2022-09-02 11:39:14 +0700

    java_concatenation

---
+++ hello_world/src/java_concatenation.java
@@ -0,0 +1,9 @@
+public class java_concatenation {
+    public static void main(String[] args) {
+        String firstName = "nabila ";
+        String lastName = "basya";
+        System.out.println(firstName +""+ lastName);
+
+    }
+}
+
