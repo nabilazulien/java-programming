d7c023bd83e77daf36f8cf81f3e755a2f712159b
nabilazulien <nabilabasya.business@gmail.com>
2022-09-02 12:06:17 +0700

    java_comments

---
+++ java_programming/src/basic/java_comments.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class java_comments {
+    public static void main(String[] args) {
+        // This is a comment
+        System.out.println("hello_world");
+        // This is a comment
+        System.out.println("hello hello");
+    }
+}
