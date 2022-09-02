669a684de23bef177e7ef9fa6a15f9055b6167ed
nabilazulien <nabilabasya.business@gmail.com>
2022-09-02 12:43:47 +0700

    java_variable

---
+++ java_programming/src/basic/java_variable.java
@@ -0,0 +1,9 @@
+package basic;
+
+public class java_variable {
+    public static void main(String[] args) {
+        int myNum = 20;
+        myNum = 25; // myNum is now 25;
+        System.out.println(myNum);
+    }
+}
