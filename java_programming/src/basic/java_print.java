ba6f2c89aa3a4385f774442987fb3a416cd3de7c
nabilazulien <nabilabasya.business@gmail.com>
2022-09-02 13:01:02 +0700

    java_print

---
+++ java_programming/src/basic/java_print.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class java_print {
+    public static void main(String[] args) {
+        String firstName = "jhoe ";
+        String lastName = "hope";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+
+    }
+}
