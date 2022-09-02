d8a2b0040a5a343aac700dd993a4d9f8993ca1cd
nabilazulien <nabilabasya.business@gmail.com>
2022-09-02 10:42:07 +0700

    hello world

---
+++ java_programming/src/basic/java_operator.java
@@ -0,0 +1,17 @@
+package basic;
+
+public class java_operator {
+    public static void main(String[] args) {
+        int sum1 = 25 * 5;
+        int sum2 = sum1 + 120;
+        int sum3 = sum1 + 150;
+        int sum4 = sum2 * 2;
+        int sum5 = sum1 + sum2 - sum3 + sum4;
+        System.out.println(sum1);
+        System.out.println(sum2);
+        System.out.println(sum3);
+        System.out.println(sum4);
+        System.out.println(sum5);
+
+    }
+}
