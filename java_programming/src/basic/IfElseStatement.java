b676a15a3c05be8c7b2d997ebbedbab37807ba11
nabilazulien <nabilabasya.business@gmail.com>
2022-10-06 11:17:50 +0700

    menentukan bilangan prima menentukan hari

---
+++ java_programming/src/basic/IfElseStatement.java
@@ -0,0 +1,27 @@
+package basic;
+
+public class IfElseStatement {
+    public static void main(String[] args) {
+        //case kita check apakah umur sudah cukup apa blm
+
+
+        int umur = 20;
+        char gender = 'F'; //F
+
+
+        if (umur > 17){
+            if (gender == 'M'){
+                System.out.println("Umur laki laki sudah cukup");
+            }
+            System.out.println("Umur sudah cukup");
+        }
+
+        int hours = 12;
+        if (hours<12){
+            System.out.println("Morning");
+        }
+        {
+
+        }
+    }
+}
