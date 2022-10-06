a3ac88a923e64e71d57d5953a947f0c63dfa0848
nabilazulien <nabilabasya.business@gmail.com>
2022-10-06 11:20:06 +0700

    menentukan bilangan prima menentukan hari menentkan jumlah bilangan ganjil genap looping kawah edukasi

---
+++ java_programming/src/basic/javaLoop_KE.java
@@ -0,0 +1,9 @@
+package basic;
+
+public class javaLoop_KE {
+    public static void main(String[] args) {
+        String[] mobils = {"Honda, Toyota, Hyundai, Suzuki, Ertiga"};
+
+
+        }
+    }
