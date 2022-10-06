76d278d7bcdea03814d2c6f647616b47f8f0a474
nabilazulien <nabilabasya.business@gmail.com>
2022-10-06 11:19:30 +0700

    menentukan bilangan prima menentukan hari menentkan jumlah bilangan ganjil genap

---
+++ java_programming/src/basic/java_looping.java
@@ -0,0 +1,19 @@
+package basic;
+
+public class java_looping {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 10; i++) {
+
+            if (i % 2 == 0) {
+                System.out.println(i + " ini adalah bilangan genap ");
+            }
+
+            if (i % 2 != 0) {
+                System.out.println(i + " ini adalah bilangan ganjil");
+            }
+
+
+        }
+    }
+}
