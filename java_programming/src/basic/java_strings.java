eb196079c22cfa66a60c75d4a3e103f3423d37a4
nabilazulien <nabilabasya.business@gmail.com>
2022-10-10 09:35:23 +0700

    java string

--- java_programming/src/basic/java_strings.java
+++ java_programming/src/basic/java_strings.java
@@ -2,7 +2,7 @@ package basic;
 
 public class java_strings {
     public static void main(String[] args) {
-        String txt = "qwertyuioplkjhgfdsazxcnbvcxzasdfghjklpoiuytrewq";
+        String txt = "nabila basya zulien";
         System.out.println("The length of the txt string is:" + txt.length());
     }
 }
